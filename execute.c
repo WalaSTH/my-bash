d9647486847f0052598b63dbd6d911f0a1510818
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-09-02 20:24:09 -0300

    primera pequeña aproximacion a execute.c

--- execute.c
+++ execute.c
@@ -1,4 +1,7 @@
 #include <sys/syscall.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "command.h"
 #include "execute.h"
 #include "builtin.h"
@@ -6,5 +9,23 @@
 void execute_pipeline(pipeline apipe){
     if(builtin_is_internal(apipe)){
         builtin_exec(apipe);
+    }
+    else{
+        scommand com = pipeline_front(apipe);
+        char *command = scommand_front(com);
+        //const unsigned int s_size = scommand_length(com);
+        char *args[4];
+        args[0] = command;
+        args[1] = NULL;
+        int pid = fork();
+        if(pid == 0){
+            int exec = execvp(command, args);  
+            if(exec == -1){
+                printf("Comando inválido.\n");
+            }
+        }
+        if(pipeline_get_wait(apipe)){
+            waitpid(pid, NULL, 0);
+        }      
     }
 }
