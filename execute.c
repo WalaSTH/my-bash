117068f6bd4ccb3a1bd61cd613a67a4395d3b3a3
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-09-05 21:10:26 -0300

    Version muy mejorada de execute, esta funciona con pipelinesgit add execute.c

--- execute.c
+++ execute.c
@@ -16,53 +16,50 @@ void execute_pipeline(pipeline apipe){
         builtin_exec(apipe);
     }
     else{
-        //Vamos a ejecutar 2 scommands conectados por un pipeline
-        // y dirigiendo a un archivo 
-        int pipeline[2];
-        int pid1, pid2;
-        unsigned int n;
-        char *com_array1[100], *com_array2[100]; //VER SI USAR 1
-        scommand com1, com2;
-        if (pipe(pipeline) == -1){
-            printf("Pipe failed");
-            //Habria que volver al myBash
-        }
-        pid1 = fork();
+        scommand scom1, scom2;
+        scom1 = pipeline_front(apipe);
+        pipeline_pop_front(apipe);
+        scom2 = pipeline_front(apipe);
+        char *com_arr1[100], *com_arr2[100];
+        int fd[2];
+        pipe(fd);
+
+        int pid1 = fork();
         if(pid1 == 0){
-            //First process
-            close(pipeline[READ]);
-            com1 = pipeline_front(apipe);
-            pipeline_pop_front(apipe); //Sera un data? !MIEDO! CUIDADO!
-            n = scommand_length(com1);
+            //Child 1
+            unsigned int n = scommand_length(scom1);
             for(unsigned int i = 0u; i < n; ++i){
-                com_array1[i] = scommand_front(com1);
-                scommand_pop_front(com1);
+                com_arr1[i] = scommand_front(scom1);
+                scommand_pop_front(scom1);
             }
-            com_array1[n] = NULL;
-            dup2(pipeline[WRITE], STDOUT_FILENO);
-            close(pipeline[WRITE]);
-            execvp(com_array1[0], com_array1);
+            com_arr1[n] = NULL;
+            close(fd[READ]);
+            dup2(fd[WRITE], STDOUT_FILENO);
+            close(fd[WRITE]);
+            execvp(com_arr1[0], com_arr1);
         }
-        waitpid(pid1, NULL, 0); //Revisar-> el &
-        pid2 = fork();
-        if(pid2 == 0){
-            //Second process
-            close(pipeline[WRITE]);
-            //aca tal vez if? tenemos que ver las redirecciones
-            dup2(pipeline[READ], STDIN_FILENO);
-            close(pipeline[READ]);
-            //Modularizable
-            com2 = pipeline_front(apipe);
-            n = scommand_length(com2);
-            for(unsigned int i = 0u; i < n; ++i){
-                com_array2[i] = scommand_front(com2);
-                scommand_pop_front(com2);
+        else{
+            int pid2 = fork();
+            if (pid2 == 0){
+                //Child 2
+                unsigned int n2 = scommand_length(scom2);
+                for(unsigned int i = 0u; i < n2; ++i){
+                    com_arr2[i] = scommand_front(scom2);
+                    scommand_pop_front(scom2);
+                }
+                com_arr2[n2] = NULL;
+                close(fd[WRITE]);
+                dup2(fd[READ],STDIN_FILENO);
+                close(fd[READ]);
+                execvp(com_arr2[0], com_arr2);
             }
-            com_array2[n] = NULL;
-            execvp(com_array2[0], com_array2);
+            else{
+                //Main process
+                close(fd[0]);
+                close(fd[1]);
+                waitpid(pid1, NULL, 0);
+                waitpid(pid2, NULL, 0);
+            }
         }
-        waitpid(pid2, NULL, 0);
-        close(pipeline[READ]);
-        close(pipeline[WRITE]);
     }
 }
