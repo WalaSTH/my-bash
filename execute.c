3dfe43c4bcf0477841065f7cf731986b927266c4
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-09-09 01:23:35 -0300

    Completada implementacion para más de dos comandos con redirecciones

--- execute.c
+++ execute.c
@@ -17,60 +17,51 @@ static void fill_array(char* a[], unsigned int size, scommand scom){
     }
     a[size] = NULL;    
 }
-/*create_child -> Creates a child process that wild execute a command
-number-> indicates the position of the command on the pipeline
-elements-> indicates the total commands to execute in the pipeline
-fd[] -> the pipe
+/*create_child_process -> Creates a child process that wild execute a command
+fd[] -> the pipe to write to
+prev -> the previous pipe to read from
+last -> indicates if we are executing the last process in pipeline
 scom -> the scomand to execute
 */
-static pid_t create_child(unsigned int number, unsigned int elements,int fd[], scommand scom){
+static pid_t create_child_process(int fd[], int *prev,bool last, scommand scom){
     pid_t pid = fork();
+    unsigned int n = scommand_length(scom);
     char* a[100];
     if(pid == -1){
         printf("Error while forking sub-process\n");
     }
     else if(pid == 0){
-        unsigned int size = scommand_length(scom);
-        fill_array(a, size, scom);
+        if (*prev != STDIN_FILENO){ 
+            //Not first command case, will read from pipe
+            dup2(*prev,STDIN_FILENO);
+            close(*prev);
+        }
+        if(!last){
+            //In the middle commands will write to pipe
+            dup2(fd[WRITE],STDOUT_FILENO);
+            close(fd[WRITE]);
+        }
         char* fileIn =  scommand_get_redir_in(scom);
         if(fileIn != NULL){ //Check if we have a redir in
             int f = open(fileIn,O_RDONLY | O_CREAT,0777);
             dup2(f,STDIN_FILENO);
         }
-        char* fileOut =  scommand_get_redir_in(scom);
+        char* fileOut =  scommand_get_redir_out(scom);
         if(fileOut != NULL){ //Check if we have a redir out
             int f = open(fileOut,O_WRONLY | O_CREAT,0777);
             dup2(f,STDOUT_FILENO);
         }
-        if(number == 1 && number == elements){
-            //Single command case
-            //Will not read from nor write to pipe
-        }
-        else if(number == 1){
-            //First of many commands case
-            //Wil only write to pipe
-            dup2(fd[WRITE], STDOUT_FILENO);
-        }
-        else if(number == elements){
-            //Last of many commands case
-            //Will only read from pipe
-            dup2(fd[READ],STDIN_FILENO);
-        }
-        else if (number != 1 && number != elements){
-            //In the middle of commands case
-            //Will both read from and write to pipe
-            dup2(fd[READ],STDIN_FILENO);            
-            dup2(fd[WRITE], STDOUT_FILENO);
-            
-        }
-        close(fd[READ]);
-        close(fd[WRITE]);
+        fill_array(a,n,scom);
         int exec = execvp(a[0],a);
         if(exec == -1){
             printf("Error executing process '%s'\n", a[0]);
             _exit(1);
         }
     }
+    //closing files we don't need on parent process
+    close(*prev);
+    close(fd[WRITE]);
+    *prev = fd[READ];
     return pid;
 }
 
@@ -80,17 +71,26 @@ void execute_pipeline(pipeline apipe){
         builtin_exec(apipe);
     }
     else{
+        scommand scom;
         unsigned int n_commands = pipeline_length(apipe);
-        int fd[2];
+        int prev_pipe, fd[2];
         pid_t pids[100];
-        pipe(fd);
-        for(unsigned int i = 1u; i<= n_commands; ++i){
-            scommand scom = pipeline_front(apipe);
+        prev_pipe = STDIN_FILENO;
+        //Executing all commands in pipeline
+        for(unsigned int i = 0u; i< n_commands; ++i){
+            bool last = i+1u == n_commands;
+            scom = pipeline_front(apipe);
             pipeline_pop_front(apipe);
-            pids[i-1u] = create_child(i, n_commands, fd, scom);
+            pipe(fd);
+            pids[i] = create_child_process(fd, &prev_pipe, last, scom);
         }
-        close(fd[0]);
-        close(fd[1]);
+        //Restoring file descriptors
+        prev_pipe = STDERR_FILENO;
+        dup2(prev_pipe, STDIN_FILENO);
+        //close(prev_pipe) -> Doing this will create a weird bug where on the 
+        //second run of mybash it closes ??? ask teachers
+
+        //Wait
         if(pipeline_get_wait(apipe)){
             for(unsigned int i = 0; i < n_commands; ++i){
                 waitpid(pids[i], NULL, 0);
@@ -99,6 +99,3 @@ void execute_pipeline(pipeline apipe){
 
     }
 }
- 
-
-
