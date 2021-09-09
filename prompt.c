bbed0c5e3b5d58161ff3e370ec3663abc024e739
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-09-09 01:56:05 -0300

    Agregados colores para el path

--- prompt.c
+++ prompt.c
@@ -3,10 +3,19 @@
 #include <limits.h>
 #include "prompt.h"
 
+static void green(void) {
+  printf("\033[0;32m");
+}
+static void reset(void) {
+  printf("\033[0m");
+}
 void show_prompt(void){
+    green();
     char path[100000];
     getcwd(path, sizeof(path));
-    printf ("%s> ",path);
+    printf ("%s",path);
+    reset();
+    printf ("$ ");
     fflush (stdout);
 }
 
