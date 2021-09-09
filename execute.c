eb8e0df6ce9037a833f61bf0a4816b2ef39a3ba2
Maciel <maciel.salguero@mi.unc.edu.ar>
2021-09-08 22:16:10 -0300

    Modularizacion de procedimientos en execute.c, y primer intento de un bash que reciba mas de dos comandos

--- execute.c
+++ execute.c
@@ -10,78 +10,95 @@
 #define READ 0
 #define WRITE 1
 
+static void fill_array(char* a[], unsigned int size, scommand scom){
+    for(unsigned int i = 0u; i < size; ++i){
+        a[i] = scommand_front(scom);
+        scommand_pop_front(scom);
+    }
+    a[size] = NULL;    
+}
+/*create_child -> Creates a child process that wild execute a command
+number-> indicates the position of the command on the pipeline
+elements-> indicates the total commands to execute in the pipeline
+fd[] -> the pipe
+scom -> the scomand to execute
+*/
+static pid_t create_child(unsigned int number, unsigned int elements,int fd[], scommand scom){
+    pid_t pid = fork();
+    char* a[100];
+    if(pid == -1){
+        printf("Error while forking sub-process\n");
+    }
+    else if(pid == 0){
+        unsigned int size = scommand_length(scom);
+        fill_array(a, size, scom);
+        char* fileIn =  scommand_get_redir_in(scom);
+        if(fileIn != NULL){ //Check if we have a redir in
+            int f = open(fileIn,O_RDONLY | O_CREAT,0777);
+            dup2(f,STDIN_FILENO);
+        }
+        char* fileOut =  scommand_get_redir_in(scom);
+        if(fileOut != NULL){ //Check if we have a redir out
+            int f = open(fileOut,O_WRONLY | O_CREAT,0777);
+            dup2(f,STDOUT_FILENO);
+        }
+        if(number == 1 && number == elements){
+            //Single command case
+            //Will not read from nor write to pipe
+        }
+        else if(number == 1){
+            //First of many commands case
+            //Wil only write to pipe
+            dup2(fd[WRITE], STDOUT_FILENO);
+        }
+        else if(number == elements){
+            //Last of many commands case
+            //Will only read from pipe
+            dup2(fd[READ],STDIN_FILENO);
+        }
+        else if (number != 1 && number != elements){
+            //In the middle of commands case
+            //Will both read from and write to pipe
+            dup2(fd[READ],STDIN_FILENO);            
+            dup2(fd[WRITE], STDOUT_FILENO);
+            
+        }
+        close(fd[READ]);
+        close(fd[WRITE]);
+        int exec = execvp(a[0],a);
+        if(exec == -1){
+            printf("Error executing process '%s'\n", a[0]);
+            _exit(1);
+        }
+    }
+    return pid;
+}
+
 void execute_pipeline(pipeline apipe){
     assert(apipe!=NULL);
     if(builtin_is_internal(apipe)){
         builtin_exec(apipe);
     }
     else{
-        scommand scom1, scom2;
         unsigned int n_commands = pipeline_length(apipe);
-        scom1 = pipeline_front(apipe);
-        pipeline_pop_front(apipe);
-        scom2 = pipeline_front(apipe);
-        char *com_arr1[100], *com_arr2[100];
-        bool multi = n_commands > 1;
         int fd[2];
+        pid_t pids[100];
         pipe(fd);
-
-        int pid1 = fork();
-        if(pid1 == 0){
-            //Child 1            
-            unsigned int n = scommand_length(scom1);
-            for(unsigned int i = 0u; i < n; ++i){
-                com_arr1[i] = scommand_front(scom1);
-                scommand_pop_front(scom1);
-            }
-            com_arr1[n] = NULL;
-            char* fileIn =  scommand_get_redir_in(scom1);
-            if(fileIn != NULL){ //Check if we have a redir out
-                int f = open(fileIn,O_RDONLY | O_CREAT,0777);
-                dup2(f,STDIN_FILENO);
-            }
-            if(multi){
-                close(fd[READ]);
-                dup2(fd[WRITE], STDOUT_FILENO);
-                close(fd[WRITE]);
-            }
-            execvp(com_arr1[0], com_arr1);
+        for(unsigned int i = 1u; i<= n_commands; ++i){
+            scommand scom = pipeline_front(apipe);
+            pipeline_pop_front(apipe);
+            pids[i-1u] = create_child(i, n_commands, fd, scom);
         }
-        else{
-            int pid2 = 1;
-            if(multi){
-                pid2 = fork();
-            }
-            if (pid2 == 0){
-                //Child 2
-                close(fd[WRITE]);
-                unsigned int n2 = scommand_length(scom2);
-                for(unsigned int i = 0u; i < n2; ++i){
-                    com_arr2[i] = scommand_front(scom2);
-                    scommand_pop_front(scom2);
-                }
-                com_arr2[n2] = NULL;
-                dup2(fd[READ],STDIN_FILENO);
-                char* fileOut = scommand_get_redir_out(scom2);
-                if(fileOut != NULL){ //Check if we have a redir out
-                    int f = open(fileOut,O_WRONLY | O_CREAT,0777);
-                    dup2(f,STDOUT_FILENO);
-                }
-                close(fd[READ]);
-                execvp(com_arr2[0], com_arr2);
-            }
-            else{
-                //Main process
-                close(fd[0]);
-                close(fd[1]);
-
-                if(pipeline_get_wait(apipe)){
-                	waitpid(pid1, NULL, 0);
-                	if(multi){
-                		waitpid(pid2, NULL, 0);
-                	}
-                }
+        close(fd[0]);
+        close(fd[1]);
+        if(pipeline_get_wait(apipe)){
+            for(unsigned int i = 0; i < n_commands; ++i){
+                waitpid(pids[i], NULL, 0);
             }
         }
+
     }
 }
+ 
+
+
